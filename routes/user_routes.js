ed39a0f2e84ff6cf98cda83461c4975e1e67441b
bethparnell <beth.parnell@generalassemb.ly>
2021-08-29 13:52:06 -0400

    refactor

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,28 +2,32 @@ const express = require('express')
 const crypto = require('crypto')
 const passport = require('passport')
 const bcrypt = require('bcrypt')
-
-const SALT_ROUNDS = 10
-
+const User = require('../models/user')
 const { BadParamsError, BadCredentialsError } = require('../utils/custom_errors')
 
-const User = require('../models/user')
-
+const SALT_ROUNDS = 10
+const RANDOM_BYTES = 16
 const reqToken = passport.authenticate('bearer', { session: false })
 
 const router = express.Router()
 
 router.post('/sign-up', (req, res, next) => {
-    Promise.resolve(req.body.credentials)
+
+    const credentials = req.body.credentials
+
+    Promise.resolve(credentials)
     .then(credentials => {
-        if (!credentials || !credentials.password || credentials.password !== credentials.password_confirmation) {
+
+        const isValidCredentials = !credentials || !credentials.password || credentials.password !== credentials.password_confirmation
+
+        if (isValidCredentials) {
             throw new BadCredentialsError()
         }
     })
-    .then(() => bcrypt.hash(req.body.credentials.password, SALT_ROUNDS))
+    .then(() => bcrypt.hash(credentials.password, SALT_ROUNDS))
     .then(hash => {
         return {
-            email: req.body.credentials.email,
+            email: credentials.email,
             hashedPassword: hash
         }
     })
@@ -33,21 +37,21 @@ router.post('/sign-up', (req, res, next) => {
 })
 
 router.post('/sign-in', (req, res, next) => {
-    const password = req.body.credentials.password
+    const credentials = req.body.credentials
     let user 
     
-    User.findOne({ email: req.body.credentials.email })
+    User.findOne({ email: credentials.email })
     .then(record => {
         if (!record) {
             throw new BadCredentialsError()
         }
         user = record
 
-        return bcrypt.compare(password, user.hashedPassword)
+        return bcrypt.compare(credentials.password, user.hashedPassword)
     })
     .then(correctPassword => {
         if (correctPassword) {
-            const token = crypto.randomBytes(16).toString('hex')
+            const token = crypto.randomBytes(RANDOM_BYTES).toString('hex')
             user.token = token
             return user.save()
         } else {
@@ -61,17 +65,18 @@ router.post('/sign-in', (req, res, next) => {
 })
 
 router.patch('/change-password', reqToken, (req, res, next) => {
+    const passwords = req.body.passwords
     let user 
 
     User.findById(req.user.id)
     .then(record => { user = record })
-    .then(() => bcrypt.compare(req.body.passwords.old, user.hashedPassword))
+    .then(() => bcrypt.compare(passwords.old, user.hashedPassword))
     .then(correctPassword => {
-        if (!req.body.passwords.new || !correctPassword) {
+        if (!passwords.new || !correctPassword) {
             throw new BadParamsError()
         }
     })
-    .then(() => bcrypt.hash(req.body.passwords.new, SALT_ROUNDS))
+    .then(() => bcrypt.hash(passwords.new, SALT_ROUNDS))
     .then(hash => {
         user.hashedPassword = hash
         user.save()
